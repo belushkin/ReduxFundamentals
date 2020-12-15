27976dbf6e830ca352c1d7e2d9a234271b6e7d1f
Maxim Bielushkin <belushkin@gmail.com>
2020-12-15 16:24:13 +0200

    Added new func redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Provider } from 'react-redux'
 
 import './api/server'
 
@@ -13,8 +14,12 @@ import store from './store'
 // console.log('State after dispatch: ', store.getState())
 
 ReactDOM.render(
+  // Render a `<Provider>` around the entire `<App>`,
+  // and pass the Redux store to as a prop
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
